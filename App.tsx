aed24a62cca7a55c7691d51e7e48c41384fb43b4
sumit kumar <sumitsaini95488@gmail.com>
2023-12-27 23:13:17 +0530

    add all template screen file structure

--- App.tsx
+++ App.tsx
@@ -1,10 +1,13 @@
 import {StyleSheet, Text, View} from 'react-native';
 import React from 'react';
+import CustomIcon from './src/components/CustomIcon';
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>coffe shop app welcome</Text>
+      <CustomIcon name="like" size={25} />
+      <Text>coffe shop app welcome's</Text>
+      <CustomIcon name="search" size={25} />
     </View>
   );
 }
