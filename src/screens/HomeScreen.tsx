c5d15a0b9a1826f1e745a06a884086bd8717c253
sumit kumar <sumitsaini95488@gmail.com>
2024-01-07 19:03:58 +0530

    create coffee beans list

--- src/screens/HomeScreen.tsx
+++ src/screens/HomeScreen.tsx
@@ -158,7 +158,36 @@ const HomeScreen = () => {
             );
           }}
         />
-
+        <Text style={styles.coffeeBeansTitle}>Coffee Beans</Text>
+        {/* Coffee Beans List */}
+        <FlatList
+          horizontal
+          showsHorizontalScrollIndicator={false}
+          data={BeanList}
+          contentContainerStyle={[
+            styles.FlatListContainer,
+            {marginBottom: tabBarHeight},
+          ]}
+          keyExtractor={item => item.id}
+          renderItem={({item}) => {
+            return (
+              <TouchableOpacity>
+                <CoffeeCard
+                  id={item.id}
+                  index={item.index}
+                  type={item.type}
+                  roasted={item.roasted}
+                  imagelink_square={item.imagelink_square}
+                  name={item.name}
+                  special_ingredient={item.special_ingredient}
+                  average_rating={item.average_rating}
+                  price={item.prices[2]}
+                  buttonPressHandler={() => {}}
+                />
+              </TouchableOpacity>
+            );
+          }}
+        />
       </ScrollView>
     </View>
   );
